943a4d14b9dd3502141d65eeade9145f4c92711f
Pauline Maudet <paulinemaudet.dev@gmail.com>
2023-03-05 22:31:50 +0100

    Add user entity (#4)
    * Add user entity
    
    * Add user entity

---
+++ src/models/IUser.ts
@@ -0,0 +1,35 @@
+import joi from 'joi';
+
+export const CreateUserSchema = joi.object({
+    id: joi.string().required(),
+    role: joi.string().valid('admin', 'manager', 'artist').required(),
+    email: joi.string().email().required(),
+    password: joi.string().alphanum().required(),
+    pseudo: joi.string().alphanum().optional(),
+    banned: joi.boolean().optional(),
+    inscriptionDate: joi.date().required(),
+}).required();
+
+export const UpdateUserSchema = joi.object({
+    id: joi.string().optional(),
+    role: joi.string().valid('admin', 'manager', 'artist').optional(),
+    email: joi.string().email().optional(),
+    password: joi.string().alphanum().optional(),
+    pseudo: joi.string().alphanum().optional(),
+    banned: joi.boolean().optional(),
+    inscriptionDate: joi.date().optional(),
+}).required();
+
+export const UpdateAdminSchema = joi.object({
+    password: joi.string().alphanum().required(),
+}).required();
+
+export interface IUser {
+    id: string,
+    role: 'admin' | 'manager' | 'artist',
+    email: string,
+    password: string,
+    username?: string,
+    banned?: boolean,
+    inscriptionDate: Date,
+}
---
+++ src/routes/usersRoutes.ts
@@ -0,0 +1,15 @@
+import {Router} from 'express';
+import UserController from '../controllers/UserController';
+
+const usersRoutes = Router();
+
+usersRoutes.post('/register', UserController.createArtist);
+usersRoutes.post('', UserController.createManager);
+usersRoutes.get('', UserController.getAllUsers);
+usersRoutes.get('/:id', UserController.getOneUser);
+usersRoutes.put('/:id', UserController.updateUser);
+usersRoutes.put('/:id/admin', UserController.updateAdmin);
+usersRoutes.put('/:id/ban', UserController.banUser);
+usersRoutes.delete('/:id', UserController.deleteUser);
+
+export default usersRoutes;
